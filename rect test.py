86a183905951a1e84ab753b5008a6267c327926d
Benjamin Co <ben.cooper94@att.net>
2016-07-13 15:15:18 -0400

    Add files via upload
    Added the room class, there is a test room to the west. Check it out, it's cool. I promise. :D

--- rect test.py
+++ rect test.py
@@ -8,6 +8,40 @@ The moveable square will collide with the immobile one.
 The moveable square will collide with the window walls.
 """
 
+#Initialize pygame
+pygame.init()
+
+#Constant Definition:
+    #Positions:
+xpos = 300#starting position if camera skewed
+ypos = 280
+px=xpos#archaic variables for revision
+py=ypos
+speed = 3
+
+CameraX = 0#camera start
+CameraY = 0
+cX = CameraX#archaic variables for revision
+cY = CameraY
+
+winX = 640
+winY = 600
+    #Colors:
+red = (230,50,50)
+purple = (128,0,128)
+black = (0,0,0)
+chocolate = (139,69,19)
+cement = (205,197,191)
+light_green = (113,198,113)
+blue = (92,172,239)
+dark_gray = (71,71,71)
+player_blue = (100,200,250)
+bg_gray = (19,19,19)
+#For more colors see this resource: http://cloford.com/resources/colours/500col.htm or use paint
+color = red
+
+
+
 def getBounds(rec):
     """Takes in a rectangle and returns a list of the various bounds.
     [0] is x left bound, [1] is x right bound, [2] is y top bound
@@ -54,51 +88,76 @@ class Wall(Rect):
         super(Wall, self).__init__(*args, **kwargs)
         walls.append(self)
         not_player.append(self)
+        self.color = cement
 
 class Env(Rect):
     def __init__(self, *args, **kwargs):
         super(Env, self).__init__(*args, **kwargs)
         not_player.append(self)
-    
-pygame.init()
+        self.color = dark_gray
+
+class Player(Rect):
+    def __init__(self, *args, **kwargs):
+        super(Player, self).__init__(*args, **kwargs)
+        self.color = player_blue
+
+class Room(object):
+    """Class Room is a Highly Customizable Template class which can create various types of rooms. gap represents a fraction of the wall that is the door. Door is always centered."""
+    def __init__(self, x=0, y=0, w=winX, h=winY,N = False,S = False,E = False,W = False,floor_color=dark_gray,wall_color=cement,wall_thickness = 20,gap = .32):
+        chunk = (1-gap)/2 #size of a piece of the wall on a gap side
+        self.Floors = []
+        self.Floors.append(Env((x,y),(w,h)))
+        self.Walls = []
+        if N:
+            self.Walls.append(Wall((x-wall_thickness,y-wall_thickness),(w * chunk+wall_thickness,wall_thickness)))
+            self.Walls.append(Wall((x+(chunk+gap)*w,y-wall_thickness),(w * chunk+wall_thickness,wall_thickness)))
+            self.Floors.append(Env((x+chunk*w,y-wall_thickness),(w*gap,wall_thickness)))
+        else:
+            self.Walls.append(Wall((x-wall_thickness,y-wall_thickness),(w+2*wall_thickness,wall_thickness)))
+        if W:
+            self.Walls.append(Wall((x-wall_thickness,y-wall_thickness),(wall_thickness,h * chunk+wall_thickness)))
+            self.Walls.append(Wall((x-wall_thickness,y+(chunk+gap)*h),(wall_thickness,h * chunk+wall_thickness)))
+            self.Floors.append(Env((x-wall_thickness,y+chunk*h),(wall_thickness,h * gap)))
+        else:    
+            self.Walls.append(Wall((x-wall_thickness,y-wall_thickness),(wall_thickness,h+2*wall_thickness)))
+        if S:
+            self.Walls.append(Wall((x-wall_thickness,y+h),(w * chunk+wall_thickness,wall_thickness)))
+            self.Walls.append(Wall((x+(chunk+gap)*w,y+h),(w * chunk+wall_thickness,wall_thickness)))
+            self.Floors.append(Env((x+chunk*w,y+h),(w * gap,wall_thickness)))
+        else:
+            self.Walls.append(Wall((x-wall_thickness,y+h),(w+2*wall_thickness,wall_thickness)))
+        if E:
+            self.Walls.append(Wall((x+w,y-wall_thickness),(wall_thickness,h * chunk+wall_thickness)))
+            self.Walls.append(Wall((x+w,y+(chunk+gap)*h),(wall_thickness,h * chunk+wall_thickness)))
+            self.Floors.append(Env((x+w,y+chunk*h),(wall_thickness,h * gap)))
+        else:    
+            self.Walls.append(Wall((x+w,y-wall_thickness),(wall_thickness,h+2*wall_thickness)))
+        for wall in self.Walls:
+            wall.color = wall_color
+        for floor in self.Floors:
+            floor.color = floor_color
+        
+
 
 clock = pygame.time.Clock()
 
-xpos = 300#starting position if camera skewed
-ypos = 280
-px=xpos#archaic variables for revision
-py=ypos
-speed = 3
-
-CameraX = 0#camera start
-CameraY = 0
-cX = CameraX#archaic variables for revision
-cY = CameraY
-
-winX = 640
-winY = 600
 
 window = pygame.display.set_mode([winX,winY])
 camera = Rect((CameraX,CameraY),(winX,winY)) #Note!!! Currently camera doesn't effect anything after initializing the scene
 pygame.display.set_caption("Moving Box")
 
 pygame.display.flip()
-red = (230,50,50)
-purple = (128,0,128)
-black = (0,0,0)
-
-color = red
 walls = []
 not_player = [] #because of how movement works we could actually include player, however it provides more clarity as to our method if we seperate them
 
-#rectangles below
-rect1 = Wall((100 - camera.x,300 - camera.y),(20,20))
-rect2 = Rect((xpos - camera.x,ypos - camera.y), (40,40))
+#rectangles below -- NOTE!!!! Order is currently IMPORTANT, as they are drawn in order declared.
+player = Player((xpos - camera.x,ypos - camera.y), (40,40))
 floor = Env((0 - camera.x,0 - camera.y),(winX,winY))
 floor2 = Env((650 - camera.x, 0 - camera.y),(winX,winY))
-
+rect1 = Wall((100 - camera.x,300 - camera.y),(20,20))
+testRoom = Room(-700,0,340,680,True,False,False,False,light_green,blue)
 #centers camera at start
-rect2.center = camera.center#comment out to allow skewed camera
+player.center = camera.center#comment out to allow skewed camera
 
 #adds to appropriate lists !!!could make a walls class and an environment class which does this in constructor <<<Class Made>>>
 #walls.append(rect1)
@@ -161,31 +220,31 @@ while True:
                 else:
                     color = purple
     if(pygame.key.get_pressed()[K_UP]):
-        if(moveRect(rect2,0,-speed,*walls)):
+        if(moveRect(player,0,-speed,*walls)):
             #print "Not Colliding!"
-            moveRect(rect2,0,+speed)
-            camera.center = rect2.center
+            moveRect(player,0,+speed)
+            camera.center = player.center
             for obj in not_player:
                 moveRect(obj,0,speed)
     if(pygame.key.get_pressed()[K_DOWN]):
-        if(moveRect(rect2,0,speed,*walls)):
+        if(moveRect(player,0,speed,*walls)):
             #print "Not Colliding!"
-            moveRect(rect2,0,-speed)
-            camera.center = rect2.center
+            moveRect(player,0,-speed)
+            camera.center = player.center
             for obj in not_player:
                 moveRect(obj,0,-speed)
     if(pygame.key.get_pressed()[K_LEFT]):
-        if(moveRect(rect2,-speed,0,*walls)):
+        if(moveRect(player,-speed,0,*walls)):
             #print "Not Colliding!"
-            moveRect(rect2,speed,0)
-            camera.center = rect2.center
+            moveRect(player,speed,0)
+            camera.center = player.center
             for obj in not_player:
                 moveRect(obj,speed,0)
     if(pygame.key.get_pressed()[K_RIGHT]):
-        if(moveRect(rect2,speed,0,*walls)):
+        if(moveRect(player,speed,0,*walls)):
             #print "Not Colliding!"
-            moveRect(rect2,-speed,0)
-            camera.center = rect2.center
+            moveRect(player,-speed,0)
+            camera.center = player.center
             for obj in not_player:
                 moveRect(obj,-speed,0)
 #    roomBounds = getBounds(floor)
@@ -209,15 +268,14 @@ while True:
  #   camera.x = CameraX
  #   camera.y = CameraY
 
-
-    window.fill((40,50,180))
-    pygame.draw.rect(window, (200,200,200), floor)
-    pygame.draw.rect(window, (200,200,200), floor2)
-    
-    pygame.draw.rect(window, color, rect1)
-    pygame.draw.rect(window, (100,200,250), rect2)
-
+#Painting of scene:
+    window.fill(bg_gray)
+    for obj in not_player:
+        pygame.draw.rect(window, obj.color, obj)
         
+    pygame.draw.rect(window, player.color, player)
+
+#What's better, .update() or .flip()?        
     pygame.display.update()
 
 class myRect(pygame.rect):
